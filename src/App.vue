d8a5005499468a415aeded4aa91f3fe15dfd447d
karan verma <karan.verma@zoomcar.com>
2019-07-19 17:18:23 +0530

    feat: code project setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>{{interval$}}</h1>
+  </section>
+</template>
+
+<script>
+import { interval } from 'rxjs'
+
+export default {
+  subscriptions() {
+    return {
+      interval$: interval(1000),
+    }
+  },
+}
+</script>
