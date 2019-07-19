e3dc6faf0231782ad7b18e44c9a9f6061c4fefc3
karan verma <karan.verma@zoomcar.com>
2019-07-20 00:06:48 +0530

    feat: tracking mouse events

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  <section>
+  <section class="app-wrapper">
     <section class="sidebar">
-      {{file$}}
       <div class="form">
         <label for="file-upload" class="custom-file-upload">
           <i class="fa fa-cloud-upload"></i> Upload
         </label>
-        <input id="file-upload" type="file" ref="file" @change="handleFileUpload()" name="upload"/>
+        <input id="file-upload" type="file" ref="file" @change="handleFileUpload()" name="upload" />
       </div>
       <div class="assets">
         <div class="images">
@@ -14,21 +13,31 @@
         </div>
       </div>
     </section>
-    <section class="canvas"></section>
+    <section class="canvas">
+      <!-- v-for for all the text and images -->
+      <!-- text and images will be a object their respactive position in it -->
+      <img
+        ref="dragimg"
+        src="http://localhost:8000/images/uploads-1462948470584.png"
+        class="images-thumbnail"
+      />
+    </section>
   </section>
 </template>
 
 <script>
-import { merge, from, of } from 'rxjs'
+import { merge, from, of, fromEvent } from 'rxjs'
 import {
   map,
   pluck,
   switchMap,
   mapTo,
+  mergeMap,
   catchError,
   share,
   startWith,
   combineLatest,
+  takeUntil,
 } from 'rxjs/operators'
 import './style.scss'
 export default {
@@ -50,36 +59,34 @@ export default {
     },
   },
   subscriptions() {
+    const log = x => console.log(x)
+    const move$ = fromEvent(document, 'mousemove')
+    const down$ = fromEvent(document, 'mousedown')
+    const up$ = fromEvent(document, 'mouseup')
+
+    const track$ = down$.pipe(mergeMap(down => move$.pipe(takeUntil(up$))))
+    track$.subscribe(log)
+
     const file$ = this.$watchAsObservable('file', {
       immediate: false,
     }).pipe(pluck('newValue'))
-    // const prepareFile = file => {
-    //   console.log('prepareFile-->', file)
-    //   const formData = new FormData()
-    //   formData.append('file', file)
-    //   return formData
-    // }
+
     const uploadFile = file => {
-      console.log('uploadfile file -->', file)
       let formData = new FormData()
       formData.append('upload', file)
-      console.log('uploadFile-->', formData)
-          for (var key of formData.entries()) {
-        console.log(key[0] + ', ' + key[1]);
-    }
       return from(
         fetch('http://localhost:8000/uploads', {
           method: 'POST',
           body: formData,
-        }).then(res=>res.json()).catch(err=>console.log(err))
+        })
+          .then(res => res.json())
+          .catch(err => console.log(err))
       )
     }
 
-    const createLoader = url => from(fetch(url).then(res=>res.json()))
+    const createLoader = url => from(fetch(url).then(res => res.json()))
     const images$ = createLoader('http://localhost:8000/images')
-    const uploadImage$ = file$.pipe(
-      switchMap(uploadFile)
-    )
+    const uploadImage$ = file$.pipe(switchMap(uploadFile))
     return {
       file$,
       images$,
